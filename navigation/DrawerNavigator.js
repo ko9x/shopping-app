307106f43c4070c3dc5c6568e83cc549749db0d2
Sean McGillis <slcidevice@yahoo.com>
2022-04-12 17:04:23 -0600

    Added the newly created ProductsStackNavigator to the DrawerNavigator and set up the navigation

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -1,16 +1,58 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { DefaultTheme } from "@react-navigation/native";
-import { MainStackNavigator } from "./StackNavigator";
+import { DefaultTheme, DrawerActions } from "@react-navigation/native";
+import { MainStackNavigator, ProductsStackNavigator } from "./StackNavigator";
+import OrdersScreen from "../screens/OrdersScreen";
+import ShopScreen from "../screens/ShopScreen";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Colors from "../constants/Colors";
+import { useNavigation } from "@react-navigation/native";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
-    return (
-        <Drawer.Navigator>
-            <Drawer.Screen name="Shop" component={MainStackNavigator} options={{ headerShown: false }}/>
-        </Drawer.Navigator>
-    )
+  const navigation = useNavigation();
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="Shop"
+        component={MainStackNavigator}
+        options={{ headerShown: false }}
+      />
+      <Drawer.Screen
+        name="Orders"
+        component={OrdersScreen}
+        options={{
+          headerLeft: () => (
+            <Ionicons
+              name="ios-menu"
+              size={32}
+              color={Colors.primary}
+              onPress={() => {
+                navigation.dispatch(DrawerActions.toggleDrawer());
+              }}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Manage Products"
+        component={ProductsStackNavigator}
+        options={{
+          headerShown: false,
+          headerLeft: () => (
+            <Ionicons
+              name="ios-menu"
+              size={32}
+              color={Colors.primary}
+              onPress={() => {
+                navigation.dispatch(DrawerActions.toggleDrawer());
+              }}
+            />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
 };
 
 export default DrawerNavigator;
-
--- screens/OrdersScreen.js
+++ screens/OrdersScreen.js
@@ -5,6 +5,21 @@ export default function OrdersScreen(props) {
     return <Screen><Text>This is the orders screen!</Text></Screen>
 };
 
+// OrdersScreen.navigationOptions = (navigationData) => {
+//     return {
+//         headerLeft: () => (
+//             <Ionicons
+//               name="ios-menu"
+//               size={32}
+//               color={Colors.primary}
+//               onPress={() => {
+//                 navigation.openDrawer();
+//               }}
+//             />
+//           ),
+//     }
+// }
+
 const styles = StyleSheet.create({
 
 });
