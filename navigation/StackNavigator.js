9c42d16ae4c42411daca592168918292d22c09b8
Sean McGillis <slcidevice@yahoo.com>
2022-04-12 15:38:44 -0600

    Added some icons to the header and a colors constant

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -3,33 +3,44 @@ import { Button } from "react-native";
 import ShopScreen from "../screens/ShopScreen";
 import DetailScreen from "../screens/DetailScreen";
 import ShoppingCartScreen from "../screens/ShoppingCartScreen";
-import Ionicons from '@expo/vector-icons/Ionicons'
+import Colors from "../constants/Colors";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { NavigationContainer } from "@react-navigation/native";
 
 const Stack = createNativeStackNavigator();
 
-const MainStackNavigator = ({navigation}) => {
+const MainStackNavigator = ({ navigation }) => {
   return (
     <Stack.Navigator>
       <Stack.Screen
         name="ShopScreen"
         component={ShopScreen}
-        options={{headerLeft: () => (
+        options={{
+          title: "Shop",
+          headerLeft: () => (
             <Ionicons
               name="ios-menu"
               size={32}
-              onPress={() => {navigation.toggleDrawer()}}
+              color={Colors.primary}
+              onPress={() => {
+                navigation.toggleDrawer();
+              }}
             />
-        )}}
+          ),
+          headerRight: () => (
+              <Ionicons
+              name="ios-cart"
+              size={32}
+              color={Colors.primary}
+              onPress={() => {
+                  navigation.navigate('ShoppingCartScreen')
+              }}
+              />
+          )
+        }}
       />
-      <Stack.Screen
-        name="DetailsScreen"
-        component={DetailScreen}
-      />
-      <Stack.Screen
-        name="ShoppingCartScreen"
-        component={ShoppingCartScreen}
-      />
+      <Stack.Screen name="DetailsScreen" component={DetailScreen} />
+      <Stack.Screen name="ShoppingCartScreen" component={ShoppingCartScreen} options={{title: 'Shopping Cart'}} />
     </Stack.Navigator>
   );
 };
