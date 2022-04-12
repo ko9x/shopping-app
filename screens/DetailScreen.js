81f704461b00327118235b56aa6d1cf70abfd2e4
Sean McGillis <slcidevice@yahoo.com>
2022-04-12 00:10:10 -0600

    Added a console.log to show the data that is being passed in from the shopScreen

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,7 +1,9 @@
 import { View, Text, StyleSheet } from "react-native";
 import Screen from "../components/UI/Screen";
 
-export default function DetailScreen(props) {
+export default function DetailScreen({navigation, route, props}) {
+
+  console.log('details', route.params.item); //@DEBUG
   return (
     <Screen>
       <Text>This is the Detail Screen!</Text>
