67b6d46e087a39db9bca29ae81c7307c03b7ef72
Sean McGillis <slcidevice@yahoo.com>
2022-04-20 16:44:10 -0600

    Now fetching the products from firebase

--- screens/ShopScreen.js
+++ screens/ShopScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import {
   View,
   Text,
@@ -12,50 +12,40 @@ import {
 } from "react-native";
 import Screen from "../components/UI/Screen";
 import Card from "../components/UI/Card";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useFocusEffect } from "@react-navigation/native";
 import Colors from "../constants/Colors";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
-const DUMMY_DATA = [
-  {
-    id: "p1",
-    title: "An Old Tire",
-    description:
-      "Just think of all the fun things you and your family can do with an amazing product like An Old Tire",
-    price: 1.95,
-    imageAddress:
-      "https://image.shutterstock.com/image-photo/old-tire-on-white-background-260nw-1576592083.jpg",
-  },
-  {
-    id: "p2",
-    title: "Ball in a Cup",
-    description:
-      "No video game can compete with the excitement of... Ball in a Cup!",
-    price: 16.5,
-    imageAddress: "https://thumbs.dreamstime.com/z/ball-cup-toy-9211440.jpg",
-  },
-  {
-    id: "p3",
-    title: "Prescription Glasses",
-    description:
-      "These stylish Prescription Glasses are the last piece of the puzzle that you have been looking for.",
-    price: 38.99,
-    imageAddress:
-      "https://images.ray-ban.com/is/image/RayBan/805289345015__002.png?impolicy=RB_Product&width=1024&bgc=%23f2f2f2",
-  },
-  {
-    id: "p4",
-    title: "A Single Sneaker",
-    description:
-      "Why in the world would anybody spend $80 on a single shoe? I mean it doesn't even have any laces. You know what? I quit... This Place is... Yes, I guess I have been typing all this in, but who cares!?",
-    price: 79.99,
-    imageAddress:
-      'https://i.ebayimg.com/images/g/WMsAAOSw3PRfoHa3/s-l400.jpg',
-  },
-];
-
 export default function ShopScreen({ navigation, route, props }) {
   const { isEditable } = route.params ? route.params : false;
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const response = await fetch('https://react-http-max-54195-default-rtdb.firebaseio.com/products.json');
+
+    const data = await response.json();
+
+    let arr = [];
+
+    for (const key in data) {
+      arr.push({
+        id: key,
+        title: data[key].title,
+        description: data[key].description,
+        price: data[key].price,
+        imageAddress: data[key].imageAddress,
+      });
+    }
+
+    setProducts(arr);
+
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      getProducts();
+    }, [])
+  )
 
   useEffect(() => {
     if (isEditable) {
@@ -110,7 +100,7 @@ export default function ShopScreen({ navigation, route, props }) {
   return (
     <Screen>
       <FlatList
-        data={DUMMY_DATA}
+        data={products}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
       />
