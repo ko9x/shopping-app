b5ff100355106e095c4793a95ba1c76db4db4e1c
Sean McGillis <slcidevice@yahoo.com>
2022-04-26 00:30:53 -0600

    Refactored addItemToCart function

--- store/Cart.js
+++ store/Cart.js
@@ -11,7 +11,10 @@ export const CartProvider = ({ children }) => {
       addItemToCart: (newItem) => {
         let existingItem = cartItems.find((item) => item.id === newItem.id);
         if (!existingItem) {
-          return setCartItems((prevItems) => [...prevItems, newItem]);
+          return setCartItems((prevItems) => [
+            ...prevItems,
+            { ...newItem, quantity: 1 },
+          ]);
         }
         if (existingItem.quantity) {
           const tempArr = {
@@ -23,12 +26,6 @@ export const CartProvider = ({ children }) => {
           };
           return setCartItems(tempArr.cartItems);
         }
-        const tempArr = {
-          cartItems: cartItems.map((item) =>
-            item.id === existingItem.id ? { ...item, quantity: 2 } : item
-          ),
-        };
-        return setCartItems(tempArr.cartItems);
       },
       removeItemFromCart: (id) => {
         let arr = cartItems;
