0b2151f83a61e0882beed05a7611dcd81f2c9d0c
Sean McGillis <slcidevice@yahoo.com>
2022-04-27 19:42:26 -0600

    Implemented fetching the orders from firebase and displaying the orders in a flatlist

--- screens/OrdersScreen.js
+++ screens/OrdersScreen.js
@@ -1,26 +1,66 @@
-import { View, Text, StyleSheet } from 'react-native';
-import Screen from '../components/UI/Screen';
+import { useCallback, useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import Screen from "../components/UI/Screen";
+import Card from "../components/UI/Card";
+import { useFocusEffect } from "@react-navigation/native";
 
 export default function OrdersScreen(props) {
-    return <Screen><Text>This is the orders screen!</Text></Screen>
-};
+  const [orders, setOrders] = useState([]);
 
-// OrdersScreen.navigationOptions = (navigationData) => {
-//     return {
-//         headerLeft: () => (
-//             <Ionicons
-//               name="ios-menu"
-//               size={32}
-//               color={Colors.primary}
-//               onPress={() => {
-//                 navigation.openDrawer();
-//               }}
-//             />
-//           ),
-//     }
-// }
+  const getOrders = async () => {
+    const response = await fetch(
+      "https://react-http-max-54195-default-rtdb.firebaseio.com/orders.json"
+    );
 
-const styles = StyleSheet.create({
+    const data = await response.json();
 
-});
+    let arr = [];
 
+    for (const key in data) {
+      arr.push({
+        id: key,
+        order: data[key].cartItems,
+      });
+    }
+
+    setOrders(arr);
+  };
+
+  useFocusEffect(
+      useCallback(() => {
+        getOrders();
+      },[])
+  )
+
+//   useEffect(() => {
+//     getOrders();
+//   }, []);
+
+  const renderItems = ({ item }) => {
+    console.log("item", item.order); //@DEBUG
+    const itemsDisplay = item.order.map((item) => (
+      <View key={item.id} style={{paddingTop: 5, flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Text>Name: {item.title}</Text>
+        <Text> Qty: {item.quantity}</Text>
+      </View>
+    ));
+    return (
+      <View style={{paddingVertical: 20}}>
+        <Text>Order number: {item.id}</Text>
+        {itemsDisplay}
+      </View>
+    );
+  };
+
+  return (
+    <Screen>
+      <FlatList
+        data={orders}
+        renderItem={renderItems}
+        keyExtractor={(item) => item.id}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
